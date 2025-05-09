fec8f8d005035c8b2fd5ffa8153ad6bd9053e42f
nik slg <barmagloth1976@gmail.com>
2025-05-09 19:25:40 +0300

    Начальная версия ToDo-app

---
+++ task_manager.py
@@ -0,0 +1,25 @@
+task_list = []
+
+def add_task(task):
+    """добавляем задачу в список"""
+    task_list.append(task)
+    
+def list_tasks():
+    """вывод всех задач с нумерацией"""
+    for i, task in enumerate(task_list, start=1):
+        print(f'{i} {task}')
+
+def delete_task(delete_ind):
+    """удаление задачи по номеру"""
+    try:
+        if 1 <= delete_ind <= len(task_list):
+            task_list.pop(delete_ind - 1)
+    except IndexError:
+        print("Введено неверное число.")
+    except ValueError:
+        print("Неверный формат данных. Введите число: ")
+        
+    
+    
+task = str(input())
+
